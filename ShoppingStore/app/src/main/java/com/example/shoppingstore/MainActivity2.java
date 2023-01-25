da0241315a6585f30fb8468ae36e3c873375c6cb
Shubham5250 <shubhampaw312@gmail.com>
2023-01-25 23:47:33 +0530

    splash-screen-edited

---
+++ ShoppingStore/app/src/main/java/com/example/shoppingstore/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.shoppingstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.shoppingstore.ui.dashboard.DashboardFragment;
+import com.example.shoppingstore.ui.home.HomeFragment;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
